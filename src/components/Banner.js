763888f93a8d4f24fc90c1a1900899c6f600b94c
March Boonyapaluk <kboonyap@cs.brown.edu>
2019-10-28 22:51:05 -0400

    initial website rework

---
+++ src/components/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Banner = (props) => (
+    <section id="banner" className="major">
+        <div className="inner">
+            <header className="major">
+                <h1>Khemarat (March) Boonyapaluk</h1>
+            </header>
+            <div className="content">
+                <p>Hi there! Welcome to my personal website.<br />
+                Feel free look around :) </p>
+                <ul className="actions">
+                    <li><a href="#one" className="button next scrolly">About me</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+)
+
+export default Banner
