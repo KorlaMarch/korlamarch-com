e43eff3f0baae688b98f741a38621e3b62302b00
Khemarat Boonyapaluk <korla.march@gmail.com>
2020-01-26 17:01:30 -0500

    add files

---
+++ src/pages/about.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+
+export const AboutPageTemplate = ({ title, content }) => {
+  return (
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                {title}
+              </h2>
+              <div className="content">{content}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+AboutPageTemplate.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string,
+}
+
+const AboutPage = () => {
+  return (
+    <Layout isNav>
+      <AboutPageTemplate
+        title="Khemarat Boonyapaluk"
+        content="placeholder"
+      />
+    </Layout>
+  )
+}
+
+export default AboutPage
